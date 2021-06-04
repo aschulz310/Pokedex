407394c3844a03b5b7056dd47800c21fc3a47873
Andy S <aschulz310@gmail.com>
2021-06-04 11:30:27 -0400

    Added styling
    Added background color, text color, and font size attributes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 let pokemonList = [
   {name: 'Venusaur', height: 2, types: ['grass' , 'poison']},
-  {name: 'Butterfree' , height: 1.1, types: ['bug' , 'flying']},
-  {name: 'Zubat' , height: 0.8, types: ['poison', 'flying']}
+  {name: 'Butterfree', height: 1.1, types: ['bug' , 'flying']},
+  {name: 'Zubat', height: 0.8, types: ['poison' , 'flying']}
 ];
+
+for (let i=0; i < pokemonList.length; i++) {
+  //Writes Pokemon name to the DOM
+  document.write(pokemonList[i].name + " (height :" + pokemonList[i].height + ")");
+//Checks if pokemon height is greater than 1.5 m
+  if (pokemonList[i].height > 1.5) {
+    //if pokemon height is greater than 1.5 m, writes "Wow that's big!"
+    document.write(" - Wow, that's big!");
+  }
+  //Adds line breaks after each pokemon name
+  document.write("<br><br>");
+}
