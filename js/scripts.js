263826bef3bed350e08a67c0d2c9a6fc51c1bf8b
Andy S <aschulz310@gmail.com>
2021-06-17 14:59:10 -0400

    Updated scripts.js to include an event listener
    Also updated styles.css to include button styling and index.html to include an empty list

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,45 @@ let pokemonRepository = (function () {
     {name: 'Zubat', height: 0.8, types: ['poison' , 'flying']}
     ];
 
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
+    function add(pokemon) {
+      if (
+        typeof pokemon === "object" &&
+        "name" in pokemon &&
+        "height" in pokemon &&
+        "types" in pokemon
+      ) {
+        pokemonList.push(pokemon);
+      } else {
+        console.log("pokemon is not correct");
+      }
+    }
 
   function getAll() {
     return pokemonList;
   }
 
+  function addListItem(pokemon) {
+    let fullList = document.querySelector('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    fullList.appendChild(listpokemon);
+    button.addEventListener('click', function (event) {
+        showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon){
+    console.log(pokemon.name);
+  }
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 
 })();
@@ -34,5 +62,5 @@ let pokemonRepository = (function () {
 
 
 pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write(pokemon.name + " " + "(height : " + pokemon.height + ")<br><br>");
+  pokemonRepository.addListItem(pokemon);
 });
